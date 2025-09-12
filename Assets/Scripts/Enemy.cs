6273d0a6cd1d973efde9904b20df0e82eec8ad0b
FreakyPerfectCell <Mlizardi1225@gmail.com>
2025-09-11 23:01:21 -0400

    Working on enemy
    works but not exactly how I want it to
    need to figure out how it'll avoid tiles

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,15 +3,31 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
-    public float speed;
+    [SerializeField] private float _speed;
+    private Rigidbody2D rb;
+    private Transform player;
 
-    void Start()
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (player == null) return;
+
+        Vector2 direction = player.position - transform.position;
+
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+        {
+            direction = new Vector2(Mathf.Sign(direction.x), 0);
+        }
+        else
+        {
+            direction = new Vector2(0, Mathf.Sign(direction.y));
+        }
+
+        rb.MovePosition(rb.position + direction * _speed * Time.deltaTime);
     }
 }
