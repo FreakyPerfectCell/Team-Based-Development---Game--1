ca9f653bea668236f235fa22e8759de5eff4c442
FreakyPerfectCell <Mlizardi1225@gmail.com>
2025-09-17 12:16:06 -0400

    little crap
    like a little to the enemy script
    set up some variables

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,6 +3,12 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Health Crap")]
+    [SerializeField] public int currentHealth;
+    [SerializeField] public int maxHealth;
+
+
+
     [Header("Movement Crap")]
     [SerializeField] private float _speed;
     private Rigidbody2D rb;
